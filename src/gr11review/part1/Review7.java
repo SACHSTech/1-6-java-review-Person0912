f8f3de6c0d7bef3322e5ccdd86190629f2553187
Person0912 <kayla.jin25@ycdsbk12.ca>
2024-09-16 10:46:21 -0400

    Mostly finished review 7

--- src/gr11review/part1/Review7.java
+++ src/gr11review/part1/Review7.java
@@ -3,7 +3,31 @@ import java.io.*;
 
 public class Review7 {
     public static void main(String[] args) throws IOException {
+        
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+        
+        String strSentence = keyboard.readLine();
+        int intSpaceCount = 0;
+        int intACount = 0;
+        String strOdd = "";
 
+        for (int i = 0; i < strSentence.length(); i++) {
+            
+            if (strSentence.charAt(i) == ' ') {
+                intSpaceCount ++;
+            }
+            if (strSentence.charAt(i) == 'a') {
+                intACount ++;
+            }
+            if (i % 2 == 0) {
+                strOdd += "-";
+            }
+        }
+
+        System.out.println("There are " + strSentence.length() + " characters in the sentence.");
+        System.out.println("There are " + intSpaceCount + " spaces in the sentence.");
+        System.out.println("There are " + intACount + " letter a in the sentence.");
+        System.out.println(strOdd);
 
     }
 }
