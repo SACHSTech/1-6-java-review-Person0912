9ad8d8ca2da87be0632f863809303bd8d8222fe1
Person0912 <kayla.jin25@ycdsbk12.ca>
2024-09-16 10:14:17 -0400

    Mostly finished review 5

--- src/gr11review/part1/Review5.java
+++ src/gr11review/part1/Review5.java
@@ -4,16 +4,16 @@ import java.io.*;
 public class Review5 {
     public static void main(String[] args) throws IOException {
        
-        // WHICH MATH FORMULA IS THIS USING AHHHHH
-        double dblInvested;
+        double dblInvYear;
         double dblIntRate;
         double dblTarg;
-        double dblTime;
+        double dblTotal = 0;
+        int intTime = 0;
 
         BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
         
         System.out.print("Enter the yearly invested amount: ");
-        dblInvested = Double.parseDouble(keyboard.readLine());
+        dblInvYear = Double.parseDouble(keyboard.readLine());
 
         System.out.print("Enter the compound interest rate: ");
         dblIntRate = Double.parseDouble(keyboard.readLine());
@@ -21,9 +21,12 @@ public class Review5 {
         System.out.print("Enter the target amount: ");
         dblTarg = Double.parseDouble(keyboard.readLine());
 
-        dblTime = Math.log(dblTarg / dblInvested) / Math.log(1 + dblIntRate);
+        while (dblTarg > dblTotal) {
+            dblTotal = (dblTotal + dblInvYear) * (1 + dblIntRate/100);
+            intTime ++;
+        }
 
-        System.out.println("The target amount will be earned in " + dblTime + " years.");
+        System.out.println("The target amount will be earned in " + intTime + " years.");
 
     }
 }
