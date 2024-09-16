32769573e598e60dc3727e1fc72f28c345c7bfb5
Person0912 <kayla.jin25@ycdsbk12.ca>
2024-09-16 09:56:59 -0400

    Mostly finished review 3

---
+++ src/gr11review/part1/Review3.java
@@ -0,0 +1,14 @@
+package gr11review.part1;
+
+public class Review3 {
+    public static void main(String[] args) throws Exception {
+
+        for (int i = 21; i <= 99; i += 2) {
+            System.out.println(i);
+        }
+        System.out.println("");
+        for (int j = 29; j >= 2; j--) {
+            System.out.println(j);
+        }
+    }
+}
